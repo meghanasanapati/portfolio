b44e47f37302b6f595b9b155654dec027f6cfa79
bvsaimanvith <bvsaimanvith@gmail.com>
2025-04-20 22:02:22 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/meg-portfolio/", // <-- your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
